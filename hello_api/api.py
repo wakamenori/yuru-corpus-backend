fed1f46b9853eacb1661b8040789148c61e67a49
wakamenori <firpmnfiywl@gmail.com>
2022-10-25 11:08:14 +0900

    wrap

--- hello_api/api.py
+++ hello_api/api.py
@@ -20,7 +20,7 @@ BASE_PATH = os.environ.get("BASE_PATH", "hello")  # TODO: change base path
 def main():
     if request.method == "GET":
         rh = ResponseHandler()
-        return rh.response_2xx(f"Hello, World!prod stage:{STAGE}")
+        return rh.response_2xx(f"Hello, World! prod stage:{STAGE}")
     if request.method == "POST":
         rh = ResponseHandler()
         data = request.get_json()
