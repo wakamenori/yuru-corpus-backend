8bcfeb273f7e165e8949c96156c3488ee60cfe7d
wakamenori <firpmnfiywl@gmail.com>
2023-01-02 22:40:51 +0900

    最新データを追加

--- data/code/manage_db.py
+++ data/code/manage_db.py
@@ -66,49 +66,35 @@ def create_table(db, table_name):
     )
 
 
-def add_data(db, table_name, data_dir):
-    summary = pd.read_csv(os.path.join(data_dir, "summary_processed.csv"))
-    summary = summary.fillna("")
-    morphemes_df = pd.read_csv(os.path.join(data_dir, "morphemes_processed.csv"))
-    morphemes_df = morphemes_df.fillna("")
-
+def add_data(db, table_name, summary_path=None, morphemes_path=None):
     table = db.Table(table_name)
-    morphemes_default_columns = [
-        "Type",
-        "Id",
-        "Speaker",
-        "Token",
-        "TokenKana",
-        "DictionaryForm",
-        "DictionaryFormKana",
-    ]
-    for default_column in morphemes_default_columns:
-        if default_column not in morphemes_df.columns:
-            morphemes_df[default_column] = np.NAN
 
     with table.batch_writer() as batch:
-        for episode in tqdm(summary.itertuples(), total=len(summary)):
-            batch.put_item(
-                Item={
-                    "Type": "Episode",
-                    "Id": str(episode.Id),
-                    "Title": episode.Title,
-                    "PublicationDate": episode.PublicationDate,
-                    "ThumbnailUrl": episode.ThumbnailUrl,
-                    "VideoUrl": episode.VideoUrl,
-                    "Channel": episode.Channel,
-                    "IsAnalyzed": episode.IsAnalyzed,
-                }
-            )
+        if summary_path is not None:
+            summary = pd.read_csv(summary_path)
+            summary = summary.fillna("")
+            for episode in tqdm(summary.itertuples(), total=len(summary)):
+                batch.put_item(
+                    Item={
+                        "Type": "Episode",
+                        "Id": str(episode.Id),
+                        "Title": episode.Title,
+                        "PublicationDate": episode.PublicationDate,
+                        "ThumbnailUrl": episode.ThumbnailUrl,
+                        "VideoUrl": episode.VideoUrl,
+                        "Channel": episode.Channel,
+                        "IsAnalyzed": episode.IsAnalyzed,
+                    }
+                )
 
         def write(
-                morpheme_id,
-                token,
-                speaker,
-                token_kana,
-                dictionary_form,
-                dictionary_form_kana,
-                pbar,
+            morpheme_id,
+            token,
+            speaker,
+            token_kana,
+            dictionary_form,
+            dictionary_form_kana,
+            pbar,
         ):
             pbar.update(1)
             if morpheme_id == "1#00:00:00#0":
@@ -132,25 +118,43 @@ def add_data(db, table_name, data_dir):
                 print("##############################")
                 print(item)
 
-        with tqdm(total=len(morphemes_df)) as progress_bar:
-            np.vectorize(write)(
-                morphemes_df["Id"],
-                morphemes_df["Token"],
-                morphemes_df["Speaker"],
-                morphemes_df["TokenKana"],
-                morphemes_df["DictionaryForm"],
-                morphemes_df["DictionaryFormKana"],
-                progress_bar,
-            )
+        if morphemes_path is not None:
+            morphemes_df = pd.read_csv(morphemes_path)
+            morphemes_df = morphemes_df.fillna("")
+
+            morphemes_default_columns = [
+                "Type",
+                "Id",
+                "Speaker",
+                "Token",
+                "TokenKana",
+                "DictionaryForm",
+                "DictionaryFormKana",
+            ]
+            for default_column in morphemes_default_columns:
+                if default_column not in morphemes_df.columns:
+                    morphemes_df[default_column] = np.NAN
+
+            with tqdm(total=len(morphemes_df)) as progress_bar:
+                np.vectorize(write)(
+                    morphemes_df["Id"],
+                    morphemes_df["Token"],
+                    morphemes_df["Speaker"],
+                    morphemes_df["TokenKana"],
+                    morphemes_df["DictionaryForm"],
+                    morphemes_df["DictionaryFormKana"],
+                    progress_bar,
+                )
 
 
 def main():
     parser = argparse.ArgumentParser(description="Create DynamoDB table")
-    parser.add_argument("table_name", help="table name")
+    parser.add_argument("table_name", help="Table name to create or add data")
     parser.add_argument("--remote", help="Deploy to remote", action="store_true")
     parser.add_argument("--delete", help="Delete existing table", action="store_true")
     parser.add_argument("--create", help="Create table", action="store_true")
-    parser.add_argument("--data_dir", help="Add data", type=str)
+    parser.add_argument("--summary_path", help="Add data", type=str)
+    parser.add_argument("--morphemes_path", help="Add data", type=str)
     args = parser.parse_args()
 
     if args.remote:
@@ -174,8 +178,8 @@ def main():
     if args.create:
         create_table(db, args.table_name)
 
-    if args.data_dir is not None:
-        add_data(db, args.table_name, args.data_dir)
+    if args.summary_path is not None or args.morphemes_path is not None:
+        add_data(db, args.table_name, args.summary_path, args.morphemes_path)
 
 
 if __name__ == "__main__":
